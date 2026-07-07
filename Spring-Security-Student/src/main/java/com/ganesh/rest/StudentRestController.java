f2c5bf878d8b2062b041926cd100ff1f10ba4ed7
Ganesh <ganeshgarje716@gmail.com>
2026-07-07 22:39:52 +0530

    Create StudentRestController Class

---
+++ Spring-Security-Student/src/main/java/com/ganesh/rest/StudentRestController.java
@@ -0,0 +1,10 @@
+package com.ganesh.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/student")
+public class StudentRestController {
+
+}
