d2d69837fa48dcfe02ce73614fce5e284a59254f
Ganesh <ganeshgarje716@gmail.com>
2026-07-08 23:16:17 +0530

    Create User Variables

--- Spring-Security-Student/src/main/java/com/ganesh/entity/User.java
+++ Spring-Security-Student/src/main/java/com/ganesh/entity/User.java
@@ -1,10 +1,26 @@
 package com.ganesh.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
 public class User {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String username;
+	
+	private String password;
+	
+	private String role;
+	
+	
 
 }
