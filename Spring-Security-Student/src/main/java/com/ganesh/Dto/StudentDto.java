16771f62799ae1f0a15b06b959240714a0ec5df5
Ganesh <ganeshgarje716@gmail.com>
2026-07-08 22:16:05 +0530

    Create Variables And Add Validations

--- Spring-Security-Student/src/main/java/com/ganesh/Dto/StudentDto.java
+++ Spring-Security-Student/src/main/java/com/ganesh/Dto/StudentDto.java
@@ -1,8 +1,53 @@
 package com.ganesh.Dto;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class StudentDto {
+	
+	
+	
+	
+	private Integer id;
+	
+	@NotNull(message = "First Name is Required")
+	private String firstName;
+	
+	@NotNull(message = "Last Name is Required")
+	private String lastName;
+
+	@NotNull(message = "Email is Required")
+	private String email;
+	
+	@NotNull(message = "Mobile No is Required")
+	private String mobileNo;
+	
+	@NotNull(message = "Date of Birth is Required")
+	private String dob;
+	
+	@NotNull(message = "Department is Required")
+	private String department;
+	
+	@NotNull(message = "Course is Required")
+	private String course;
+	
+	private String registerDate;
+	
+	@NotNull(message = "SSC Marks is Required")
+	private Integer sscMarks;
+	
+	@NotNull(message = "HSC Marks is Required")
+	private Integer hscMarks;
+	
+	@NotNull(message = "Address is Required")
+	private String address;
+	
+	@NotNull(message = "Age is Required")
+	private Integer age;
 
 }
