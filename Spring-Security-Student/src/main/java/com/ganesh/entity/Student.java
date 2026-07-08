c2c248a267ef940d77703d010910ea364d651769
Ganesh <ganeshgarje716@gmail.com>
2026-07-08 22:15:36 +0530

    Create Variables and Add Annotations

--- Spring-Security-Student/src/main/java/com/ganesh/entity/Student.java
+++ Spring-Security-Student/src/main/java/com/ganesh/entity/Student.java
@@ -1,6 +1,12 @@
 package com.ganesh.entity;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +16,37 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 public class Student {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	@Column(unique = true)
+	private String email;
+	
+	@Column(unique = true)
+	private String mobileNo;
+	
+	private String dob;
+	
+	private String department;
+	
+	private String course;
+	
+	@CreationTimestamp
+	private String registerDate;
+	
+	private Integer sscMarks;
+	
+	private Integer hscMarks;
+	
+	private String address;
+	
+	private Integer age;
 
 }
