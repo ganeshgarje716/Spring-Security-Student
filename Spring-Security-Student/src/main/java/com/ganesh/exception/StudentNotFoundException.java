4dd455379265ff083a15b84fba13920b3502a19c
Ganesh <ganeshgarje716@gmail.com>
2026-07-08 23:46:56 +0530

    Create StudentNotFoundException Class

---
+++ Spring-Security-Student/src/main/java/com/ganesh/exception/StudentNotFoundException.java
@@ -0,0 +1,12 @@
+package com.ganesh.exception;
+
+public class StudentNotFoundException extends RuntimeException {
+	
+	
+	public StudentNotFoundException(String msg) {
+		
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+
+}
